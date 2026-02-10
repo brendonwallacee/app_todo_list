095a1d69e3adf1d296bb2c686b38cd76c1913e21
Brendon <brendonwallace2001@gmail.com>
2026-02-10 17:33:41 -0300

    html (request + test)

--- api_acess_alterdata/app.py
+++ api_acess_alterdata/app.py
@@ -1,12 +1,24 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from api_acess_alterdata.schemas import Message
 
 app = FastAPI(title='API de acesso ao Alterdata')
 
 
-@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message,)
 def read_root():
     return {'message': 'Olá mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_html():
+    return """
+    <html>
+      <body>
+        <h1>Olá mundo!</h1>
+      </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,20 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá mundo!'}
+
+
+def test_deve_retornar_ola_mundo_em_html():
+    client = TestClient(app)
+
+    response = client.get('/html')
+
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+    <html>
+      <body>
+        <h1>Olá mundo!</h1>
+      </body>
+    </html>"""
+    )
